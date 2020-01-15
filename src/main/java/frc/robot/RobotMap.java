7fbb3b12874f452b3501e81602bc66891c4dda3b
Kshitij Teotia <teotia.kshitij@gmail.com>
2020-01-14 18:01:59 -0800

    RobotMap

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,11 @@
+package frc.robot;
+
+public class RobotMap{
+    public static final int L1_ID = 0;
+	public static final int L2_ID = 0;
+	public static final int R1_ID = 0;
+    public static final int R2_ID = 0;
+    public static final int JOYSTICK_PORT = 0;
+    public static final int JOYSTICK_Y = 0;
+    public static final int JOYSTICK_X = 0;
+}
